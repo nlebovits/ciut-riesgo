ecab6b6d5f113ddb2a86c91bb2b5aac5e6868488
nlebovits <nissim.lebovits@vanderbilt.edu>
2025-08-19 20:27:27 -0300

    get clean doc, add initial viz

--- config.py
+++ config.py
@@ -8,30 +8,38 @@ import contextily as ctx
 # Continuous color palettes
 # Blue palette (5 sequential shades)
 BLUE_PALETTE = [
-    '#0d1b4d',  # Darkest blue
-    '#1a2e5c',  # Dark blue
-    '#2d4b73',  # Medium-dark blue
-    '#4d7b99',  # Medium blue
-    '#7ba3c2'   # Light blue
+    '#0b2c75',  # Darkest blue
+    '#4d5496',  # Dark blue
+    '#7f7fb8',  # Medium-dark blue
+    '#afaedb',  # Medium blue
+    '#e1dfff'   # Light blue
 ]
 
 # Pink palette (5 sequential shades)
 PINK_PALETTE = [
-    '#4d0d1b',  # Darkest pink
-    '#6a1a2e',  # Dark pink
-    '#8a2d4b',  # Medium-dark pink
-    '#a94d7b',  # Medium pink
-    '#c27ba3'   # Light pink
+    '#750b40',  # Darkest pink
+    '#984466',  # Dark pink
+    '#bb748f',  # Medium-dark pink
+    '#dda4b9',  # Medium pink
+    '#ffd5e5'   # Light pink
 ]
 
-# Binary palette: blue -> white -> pink
-BINARY_PALETTE = ['#0d1b4d', '#ffffff', '#4d0d1b']
+# Divergent palette: pink -> neutral -> blue
+DIVERGENT_PALETTE = [
+    '#750b40',  # Darkest pink
+    '#a25e76',  # Dark pink
+    '#cba6b1',  # Light pink
+    '#f1f1f1',  # Neutral
+    '#aaa9c7',  # Light blue
+    '#65679e',  # Medium blue
+    '#0b2c75'   # Darkest blue
+]
 
 # Categorical color scheme (6 colors)
 # Blue and pink from above + 4 colors that complement the scheme
 CATEGORICAL_COLORS = [
-    '#0d1b4d',  # Blue alta
-    '#4d0d1b',  # Pink alta
+    '#0b2c75',  # Blue alta
+    '#750b40',  # Pink alta
     '#2d8a5c',  # Teal (complements both blue and pink)
     '#d17d2d',  # Warm orange (contrasts well)
     '#2d8a8a',  # Cyan (cool tone, complements the palette)
